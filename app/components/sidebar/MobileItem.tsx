2876eb1df5adc6a14622b1a413a373ce88cd6f5a
JwaSangHeon <whk4051@naver.com>
2023-07-31 09:47:34 +0900

    사이드 바 디자인 및 모바일 사이드 바(footer) 완료 및 기능 완료

---
+++ app/components/sidebar/MobileItem.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+
+interface MoblieItemProps {
+  href: string;
+  label: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const MobileItem: React.FC<MoblieItemProps> = ({
+  href,
+  label,
+  icon: Icon,
+  active,
+  onClick,
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  };
+
+  return (
+    <Link
+      className={clsx(
+        `
+      group flex gap-x-3 w-full justify-center p-4 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100
+      `,
+        active && `bg-gray-100 text-black`
+      )}
+      href={href}
+      onClick={handleClick}
+    >
+      <Icon className="w-6 h-6" />
+    </Link>
+  );
+};
+
+export default MobileItem;
